a8c0fddbced3e116f7dc823027605f5ca9cc4152
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-12-11 07:24:51 +0100

    Create sidebar.php

---
+++ member/includes/sidebar.php
@@ -0,0 +1,10 @@
+<div class="sidebar">
+    <h2>CedisPay Menu</h2>
+    <a href="dashboard.php">Dashboard</a>
+    <a href="apply_loan.php">Apply for Loan</a>
+    <a href="my_loans.php">My Loans</a>
+    <a href="payments.php">Make Payment</a>
+    <a href="profile.php">Profile</a>
+    <a href="settings.php">Settings</a>
+    <a href="logout.php">Logout</a>
+</div>
