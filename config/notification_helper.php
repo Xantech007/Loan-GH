357804c8e0be876e5caa55804c0a7de77fd8601c
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-12-13 09:30:46 +0100

    Delete config/notification_helper.php

--- config/notification_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-function createNotification($conn, $title, $message, $type, $user_id = null) {
-    $stmt = $conn->prepare("INSERT INTO notifications (user_id, title, message, type) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("isss", $user_id, $title, $message, $type);
-    $stmt->execute();
-    $stmt->close();
-}
-?>
