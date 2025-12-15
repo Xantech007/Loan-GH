2e95b628fcb93e68e53ed61f4f362155f6c9f244
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-12-15 21:54:12 +0100

    Update dashboard.php

--- members/dashboard.php
+++ members/dashboard.php
@@ -609,7 +609,7 @@ try {
 
         function logout() {
             if (confirm('Are you sure you want to log out?')) {
-                window.location.href = '../logout.php';
+                window.location.href = 'logout.php';
             }
         }
     </script>
